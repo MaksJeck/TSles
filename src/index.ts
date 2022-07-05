666b87073f36ded94a24f82446a27d2ff6192c76
MaksJeck <maks-jeck@mail.ru>
2022-07-05 16:19:42 +0600

    Home_work_1

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,38 @@
-const getArgs = (): string[] | undefined => {
-    const args = process.argv.slice(2);
-    if (args.length) {
-        return args
-    }
-    return undefined
+// const getArgs = (): string[] | undefined => {
+//     const args = process.argv.slice(2);
+//     if (args.length) {
+//         return args
+//     }
+//     return undefined
+// };
+
+// const forEach = <T = any>(array: T[], callback: (value: T) => void) => {
+//     array.forEach((value) => callback(value));
+// };
+
+// const main = () => {
+//     const args = getArgs();
+//     if (args) {
+//         forEach(args, console.log);
+//     } else {
+//         console.log("Аргументы не были переданы!");
+//     }
+// };
+
+// main();
+
+//-----------------------------Lesson_1---------------------------------
+// Калькулятор. Для расчёта введите в консоль, через пробел, два числа и символ
+// (/ или * или - или +) для расчёта числа
+// например: npm start 2 3 *
+const [a, b, c] = process.argv.slice(2);
+
+function calculate(a: number, b: number, c: string) {
+    if (c == "+") return a + b;
+    else if (c == "-") return a - b;
+    else if (c == "*") return a * b;
+    else if (c == "/") return a / b;
+    else return false;
 };
 
-const forEach = <T = any>(array: T[], callback: (value: T) => void) => {
-    array.forEach((value) => callback(value));
-};
-
-const main = () => {
-    const args = getArgs();
-    if (args) {
-        forEach(args, console.log);
-    } else {
-        console.log("Аргументы не были переданы!");
-    }
-};
-
-main();
+console.log(calculate(parseInt(a), parseInt(b), c));
